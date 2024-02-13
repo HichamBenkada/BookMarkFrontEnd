0c1c8260896b9b49808eebdc7ff119e7050b8517
HichamBENKADA <hicham20014@gmail.com>
2024-02-13 16:26:02 -0500

    app up-to-date

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,57 @@
-import { useState, useEffect, useRef } from 'react'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [bookmarks, setBookmarks] = useState([])
-  const [deleteToggle, setDeleteToggle] = useState(false)
-  const nameRef = useRef(null)
-  const desRef = useRef(null)
-  const linkRef = useRef(null)
+  const [bookmarks, setBookmarks] = useState([]);
+  const [deleteToggle, setDeleteToggle] = useState(false);
+  const nameRef = useRef(null);
+  const desRef = useRef(null);
+  const linkRef = useRef(null);
 
-  const BASE_URL = "https://bookmarkbackend-8k1y.onrender.com"
+  const BASE_URL = "https://bookmarkbackend-8k1y.onrender.com";
 
   useEffect(() => {
     const getBookmarks = async () => {
       try {
-        const response = await fetch(`${BASE_URL}/bookmarks`)
-        const data = await response.json()
-        setBookmarks(data)
+        const response = await fetch(`${BASE_URL}/bookmarks`);
+        const data = await response.json();
+        setBookmarks(data);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    };
 
-    getBookmarks()
-
-  }, [deleteToggle])
+    getBookmarks();
+  }, [deleteToggle]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       const body = {
         name: nameRef.current.value,
         description: desRef.current.value,
-        link: linkRef.current.value
-      }
+        link: linkRef.current.value,
+      };
 
       const response = await fetch(`${BASE_URL}/bookmarks`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(body)
-      })
-      const data = await response.json()
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
 
-      setBookmarks([...bookmarks, data])
-
+      setBookmarks([...bookmarks, data]);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-
+  };
 
   return (
     <>
       <h1>Bookmarks</h1>
-      <form style={{textAlign: "left"}} onSubmit={handleSubmit}>
+      <form style={{ textAlign: "left" }} onSubmit={handleSubmit}>
         Name: <input type="text" ref={nameRef} />
         <br />
         Description: <input type="text" ref={desRef} />
@@ -65,30 +62,42 @@ function App() {
       </form>
 
       <ul>
-        {
-          bookmarks.length ?
-            bookmarks.map((b) => {
-              return <div key={b._id}  style={{border: "2px solid white", margin: "5px", padding: "10px"}}>
+        {bookmarks.length ? (
+          bookmarks.map((b) => {
+            return (
+              <div
+                key={b._id}
+                style={{
+                  border: "2px solid white",
+                  margin: "5px",
+                  padding: "10px",
+                }}
+              >
                 <a href={b.link}>{b.name} Link</a>
                 <p>{b.description}</p>
-                <button onClick={async () => {
-                  try {
-                    await fetch(`${BASE_URL}/bookmarks/` + b._id, {
-                      method: "DELETE",
-                    })
-                    setDeleteToggle(!deleteToggle)
-                  } catch (err) {
-                    console.log(err)
-                  }
-                }}>Delete</button>
+                <button
+                  onClick={async () => {
+                    try {
+                      await fetch(`${BASE_URL}/bookmarks/` + b._id, {
+                        method: "DELETE",
+                      });
+                      setDeleteToggle(!deleteToggle);
+                    } catch (err) {
+                      console.log(err);
+                    }
+                  }}
+                >
+                  Delete
+                </button>
               </div>
-            })
-          :
-            <p>No bookmarks found :/</p>
-        }
+            );
+          })
+        ) : (
+          <p>No bookmarks found :/</p>
+        )}
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
